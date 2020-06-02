49c60efb5e8d630ae0652d96a487fda1e9845657
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-02 21:47:15 +0200

    :bulb: Add layout for authenticated routes

---
+++ layouts/auth.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="auth">
+    <left-menu />
+    <div id="auth-container">
+      <nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+import LeftMenu from '~/components/app/LeftMenu.vue';
+export default {
+  components: {
+    LeftMenu
+  }
+};
+</script>
+
+<style lang="scss">
+#auth {
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
+  display: flex;
+}
+
+#auth-container {
+  padding: 20px;
+  height: 100vh;
+  overflow-y: auto;
+  width: calc(100vw - 260px);
+}
+</style>

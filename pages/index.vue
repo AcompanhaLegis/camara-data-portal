83713370960c826f70f49b2b58bc1677c4c84949
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-02 21:46:42 +0200

    :arrow_up: Update file to match prettier

--- pages/index.vue
+++ pages/index.vue
@@ -23,13 +23,13 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
     Logo
   }
-}
+};
 </script>
 
 <style>

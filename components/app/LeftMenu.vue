936d63e595524f16e1db687e2f28e88df9edef7c
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-05 23:38:29 +0200

    :bulb: Add logo to left menu

--- components/app/LeftMenu.vue
+++ components/app/LeftMenu.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="left-menu">
+    <div class="logo">
+      <img src="~/assets/logo.svg" />
+    </div>
     <a-menu
       :default-selected-keys="[$nuxt.$route.name]"
       mode="inline"
@@ -28,10 +31,21 @@
   width: 260px;
   height: 100vh;
   overflow: hidden;
+  .logo {
+    background-color: #001529;
+    padding: 20px 0;
+    text-align: center;
+    border-bottom: 1px solid white;
+    > img {
+      max-width: 100%;
+    }
+  }
   .full-menu {
-    padding-top: 30px;
     width: 100%;
     height: 100%;
+    li:first-child {
+      margin-top: 0;
+    }
   }
 }
 </style>

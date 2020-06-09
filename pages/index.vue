b279a212774372421d91167f41012a7495cfd07c
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-09 10:00:02 +0200

    :bulb: Add sketch for the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,67 +1,49 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        camara-data-portal
-      </h1>
-      <h2 class="subtitle"></h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <main class="full-height-section">
+      <section class="btn-holder">
+        <a-button type="primary" size="large">
+          Saiba mais
+        </a-button>
+
+        <nuxt-link :to="loginRoute">
+          <a-button size="large">
+            Já sou cadastrado
+          </a-button>
+        </nuxt-link>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue';
-
 export default {
   auth: false,
-  components: {
-    Logo
+  computed: {
+    loginRoute() {
+      if (this.$auth.loggedIn) {
+        return '/proposicoes';
+      }
+      return '/login';
+    }
   }
 };
 </script>
 
-<style>
+<style lang="scss">
 .container {
   margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .full-height-section {
+    min-height: 100vh;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .btn-holder {
+    display: flex;
+    justify-content: center;
+    button {
+      margin: 10px;
+    }
+  }
 }
 </style>

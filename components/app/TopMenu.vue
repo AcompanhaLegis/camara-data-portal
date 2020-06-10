133ee54bb0215286a6c71057c41ebee1c658607a
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-10 19:48:04 +0200

    :bulb: Add profile page

---
+++ components/app/TopMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-menu
+    mode="horizontal"
+    theme="light"
+    :default-selected-keys="[$nuxt.$route.name]"
+  >
+    <a-menu-item class="item-right" key="profile">
+      <n-link to="/profile">
+        <a-icon type="user" />
+        Perfil
+      </n-link>
+    </a-menu-item>
+
+    <a-menu-item class="item-right" key="notifications">
+      <a-icon type="bell" />
+      Notificaçōes
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<style lang="scss">
+.item-right {
+  float: right;
+}
+</style>

9d2bcbd361822e241ddf7bf20bdffaaeff208201
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-06-19 23:18:11 +0200

    :bulb: Add layout for deputado speeches

--- pages/deputados/index.vue
+++ pages/deputados/index.vue
@@ -71,6 +71,19 @@
               description="Sem informaçōes"
               show-icon
             />
+            <a-list-item v-else v-for="(s, idx) in speeches" :key="idx">
+              <b>{{ s.faseEvento.titulo }}</b>
+              <br />
+              <a-tag color="cyan">
+                {{ formattedDate(s.dataHoraInicio) }}
+              </a-tag>
+              <br />
+              <b>Tipo: </b> {{ s.tipoDiscurso || '-' }}
+              <br />
+              <b>Sumário: </b> {{ s.sumario || '-' }}
+              <br />
+              <b>Transcriçāo: </b> {{ s.transcricao || '-' }}
+            </a-list-item>
           </a-list>
 
           <a-divider />

634fa23c7b0af8c39152a1394b444dea812d2267
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2020-09-07 18:41:06 +0200

    :arrow_up: Rename axios plugin to open data
    While write the contribution guidelines, this sounded very weird since we have this. and the plugin name was axios but it was implementing this.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/antd-ui', '@/plugins/axios', '@/plugins/moment'],
+  plugins: ['@/plugins/antd-ui', '@/plugins/openData', '@/plugins/moment'],
   /*
    ** Nuxt.js dev-modules
    */

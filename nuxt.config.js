9d2b5ba41da8fd8a5d433105b81f12e081a4f11a
Andressa Valadares <andressa.valadares@hepta.com.br>
2020-10-12 14:24:43 -0300

    :arrow_up: Adding fix option to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -82,6 +82,16 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {
+      config.module.rules.push({
+        enforce: 'pre',
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        exclude: /(node_modules)/,
+        options: {
+          fix: true
+        }
+      });
+    }
   }
 };

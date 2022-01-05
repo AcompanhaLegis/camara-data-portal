d9e003a8913e9a0f8a11f9e065d47919c83b2fb0
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2022-01-05 22:05:27 +0100

    Add home intiial content

---
+++ components/website/HomeInitialContent.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row id="home-initial-content">
+    <v-col cols="12" md="6" sm="12" xs="12">
+      <p class="home-initial-text">
+        Acompanha Legis é a plataforma que possibilita acompanhar proposiçōes,
+        deputados e outras atividades na camara dos deputados!
+      </p>
+    </v-col>
+
+    <v-col cols="12" md="6" sm="12" xs="12">
+      <img
+        alt="Figura ilustrativa com uma pessoa em frente a um dashboard"
+        src="~/assets/home-initial.svg"
+      />
+    </v-col>
+
+    <v-col cols="12">
+      <v-btn color="primary" large>Acompanhe aqui o governo</v-btn>
+    </v-col>
+  </v-row>
+</template>
+
+<style scoped lang="scss">
+#home-initial-content {
+  margin-top: 9%;
+  .home-initial-text {
+    font-size: 2rem;
+    font-weight: bold;
+    line-height: 1.5;
+    color: #000;
+  }
+}
+</style>

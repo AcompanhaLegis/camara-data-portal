41d6e50df634fcfcd25336f9fd34c33de5634ff9
Jose Eduardo Kimura Reis <j.eduardokimura@icloud.com>
2022-01-05 21:44:35 +0100

    Add vuetify and new header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,10 @@
-<template>
-  <div>
-    <a-menu
-      mode="horizontal"
-      theme="dark"
-      class="menu"
-      :default-selected-keys="[$nuxt.$route.name]"
-    >
-      <a-menu-item v-if="!$auth.loggedIn" key="login" class="item-right">
-        <n-link to="/login">
-          Login
-        </n-link>
-      </a-menu-item>
-
-      <a-menu-item key="proposicoes" class="item-right">
-        <n-link to="/proposicoes">
-          Proposicoes
-        </n-link>
-      </a-menu-item>
-
-      <a-menu-item key="index" class="item-right">
-        <n-link to="/">
-          Home
-        </n-link>
-      </a-menu-item>
-    </a-menu>
-    <nuxt />
-  </div>
+<template functional>
+  <nuxt />
 </template>
 
-<script>
-export default {};
-</script>
-
-<style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
+<style lang="scss">
+html,
+body {
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
@@ -51,10 +19,14 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-.menu {
-  width: 100vw;
-}
-.item-right {
-  float: right;
+
+/**
+* .v-application overwrite for development mode, 
+* threeShake on development mode prevents some components to
+* be automatically imported and it is necessary for customVariable usage.
+* https://github.com/nuxt-community/vuetify-module#treeShake
+*/
+.v-application {
+  font-family: 'IBM Plex Mono' !important;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,80 +1,50 @@
 <template>
-  <div id="home">
-    <main class="full-height-section">
-      <section class="presentation">
-        <img src="~/assets/home_laptop.png" class="home-laptop" />
-        <section class="presentation-text">
-          <h1>Acompanha Legis</h1>
-          <h2>
-            A plataforma que possibilita acompanhar proposiçōes, deputados e
-            outras atividades na camara dos deputados!
-          </h2>
-        </section>
+  <v-app>
+    <main id="home">
+      <section class="home-section first-section">
+        <v-container>
+          <v-row no-gutters>
+            <v-col cols="12" sm="12">
+              <Header />
+            </v-col>
+          </v-row>
+        </v-container>
       </section>
     </main>
-  </div>
+  </v-app>
 </template>
 
 <script>
+import Header from '@/components/website/Header.vue';
+
 export default {
-  auth: false
+  auth: false,
+  components: {
+    Header
+  },
+  layout: 'default'
 };
 </script>
 
 <style lang="scss">
 #home {
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  .full-height-section {
-    min-height: calc(100vh - 46px);
-  }
+  background-color: #f2f6ff;
 
-  .btn-holder {
-    display: flex;
-    justify-content: center;
-    button {
-      margin: 10px;
-    }
-  }
-
-  .presentation {
+  .home-section {
+    height: 100vh;
+    width: 100vw;
+    padding: 4rem 10rem 0 4rem;
     position: relative;
-    .home-laptop {
-      width: 750px;
-      position: absolute;
-      top: 80px;
-      right: 5%;
-      z-index: 99;
-    }
-    .presentation-text {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, 50%);
-      background: linear-gradient(
-        0deg,
-        rgba(24, 144, 255, 1) 0%,
-        rgba(0, 126, 245, 1) 100%
-      );
-      padding: 50px 100px;
-      width: 100vw;
-      h1 {
-        font-size: 3rem;
-        color: white;
-      }
-      h2 {
-        font-size: 2rem;
-        width: 35vw;
-      }
-    }
   }
 
-  @media screen and (max-width: 1024px) {
-    .presentation {
-      .home-laptop {
-        display: none;
-      }
+  .first-section {
+    background-color: #f2f6ff;
+  }
+}
+@media screen and (max-width: 1024px) {
+  #home {
+    .home-section {
+      padding: 10px 5px;
     }
   }
 }

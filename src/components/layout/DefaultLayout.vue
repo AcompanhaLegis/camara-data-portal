6baab555b2ab85ad9f372a60fd820efd8ffb06ca
Kimuraz <j.eduardokimura@icloud.com>
2023-04-10 14:12:37 +0200

    Move files to layout folder

---
+++ src/components/layout/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import ALFooter from "./ALFooter.vue";
+import ALHeader from "./ALHeader.vue";
+</script>
+
+<template>
+  <div class='mx-auto min-h-screen w-screen! flex flex-col overflow-y-auto overflow-x-none'>
+    <ALHeader />
+
+    <main>
+      <slot />
+    </main>
+
+    <ALFooter />
+  </div>
+</template>
+
+<style scoped>
+</style>

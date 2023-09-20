f644b99086c0bf7056d0d0322e910994b4aa2845
Kimuraz <j.eduardokimura@icloud.com>
2023-09-20 17:23:33 +0200

    Fix tab selection and comment out the agend

--- src/components/home/HomeTabs.vue
+++ src/components/home/HomeTabs.vue
@@ -1,41 +1,51 @@
 <script setup lang='ts'>
-import { computed } from "vue";
-import { useRoute } from "vue-router";
+import {computed, onMounted, ref} from "vue";
+import {useRoute, useRouter} from "vue-router";
 import { TabGroup, Tab, TabPanels, TabList, TabPanel } from "@headlessui/vue";
 import DeputadosTab from "../deputados/DeputadosTab.vue";
 import ProposicoesTab from "../proposicoes/ProposicoesTab.vue";
 
 const route = useRoute();
-const selectedTab = computed(() => {
-    const tab = route.query.tab;
-    if (tab === "proposicoes") {
-        return "Proposições";
-    } else if (tab === "deputados") {
-        return "Deputados";
-    } else if (tab === "agenda") {
-        return "Agenda";
-    } else {
-        return "Proposições";
+const router = useRouter();
+const selectedTab = ref<Number>(0);
+
+onMounted(() => {
+  const tab = route.query.tab;
+  if (tab) {
+    const index = tabs.findIndex((t) => t.route === `/?tab=${tab}`);
+    if (index >= 0) {
+      selectedTab.value = index;
     }
-});
+  }
+})
+
 const tabs = [
     { label: "Proposições", route: "/?tab=proposicoes" },
     { label: "Deputados", route: "/?tab=deputados" },
-    { label: "Agenda", route: "/?tab=agenda" }
+    //{ label: "Agenda", route: "/?tab=agenda" }
 ];
+
+const changeTab = (index: Number) => {
+  selectedTab.value = index ;
+  router.push(tabs[index].route);
+};
 </script>
 
 <template>
-  <TabGroup>
-    <TabList class='flex bg-gray-200 p-1 rounded-md text-slate-700 justify-between md:justify-start md:gap-8'>
+  <TabGroup
+      :selectedIndex="selectedTab"
+      @change='changeTab'
+  >
+    <TabList
+        class='flex bg-gray-200 p-1 rounded-md text-slate-700 justify-between md:justify-start md:gap-8'
+    >
       <Tab
-        v-for='tab in tabs'
+        v-for='(tab, idx) in tabs'
         :key='tab.label'
         :class="[
           'py-1 px-2 md:px-4 rounded-md outline-none cursor-pointer',
-          { 'bg-white text-blue-500': selectedTab === tab.label },
+          { 'bg-white text-blue-500': selectedTab === idx },
         ]"
-        @click='() => $router.push(tab.route)'
       >
         {{ tab.label }}
       </Tab>

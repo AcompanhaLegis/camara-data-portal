dee6043fc5553581a2bfd0a85874d4994f9dcb6e
Kimuraz <j.eduardokimura@icloud.com>
2023-10-10 20:15:11 +0200

    Fix missing import

--- src/components/about/__tests__/AboutAcompanhaLegis.spec.ts
+++ src/components/about/__tests__/AboutAcompanhaLegis.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it } from "vitest";
+import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 
 import AboutAcompanhaLegis from "../AboutAcompanhaLegis.vue";

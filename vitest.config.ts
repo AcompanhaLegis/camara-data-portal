535320623029701c5e9ad4e05bc5f0d18e2463ff
Kimuraz <j.eduardokimura@icloud.com>
2023-10-25 23:24:07 +0200

    Add tests for useDeputadoDetails
    This is part of #98

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
@@ -10,6 +10,9 @@ export default defineConfig({
             all: true,
             reporter: ["text", "html"],
             extension: [".vue", ".ts", ".js"],
+        },
+        alias: {
+            "@/": new URL("./src/", import.meta.url).pathname,
         },
     },
     plugins: [

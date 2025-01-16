6e78a2b6bcfb631da353c5db50189c5f87b85dc7
Kimuraz <j.eduardokimura@icloud.com>
2025-01-16 22:15:41 +0100

    Update avatar style

--- src/components/proposicoes/ProposicaoSummary.vue
+++ src/components/proposicoes/ProposicaoSummary.vue
@@ -9,11 +9,17 @@ defineProps<{
 
 <template>
   <p class="flex items-center gap-2 !text-xl">
-    <Avatar :label="proposicao.siglaTipo" class="mr-2" size="large" />
+    <Avatar
+      :label="proposicao.siglaTipo"
+      :class="['mr-2', { '!text-base': proposicao.siglaTipo.length > 2 }]"
+      size="large"
+    />
 
     {{ proposicao.numero }}/{{ proposicao.ano }}
   </p>
+
   <hr class="my-2 border-slate-800 dark:border-zinc-300" />
+
   <p>{{ proposicao.ementa }}</p>
 </template>
 

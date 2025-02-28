fbef9885aea383a881def27ad287fdb501ec77f6
ElmarRoberto <elmarcfilho@gmail.com>
2025-02-28 14:44:56 -0300

    Create api folder and update fetchCamaraAPI
    - Move BASE_URL to api folder
    - Start an enum for urls
    - Type fetchCamaraAPI

--- src/utils/fetchCamaraAPI.ts
+++ src/utils/fetchCamaraAPI.ts
@@ -1,12 +1,13 @@
-const BASE_URL = "https://dadosabertos.camara.leg.br/api/v2";
+import { BASE_URL } from "@/api";
+import { IAPIDeputado } from "@/types/IAPI";
 
-const fetchCamaraAPI = async (endpoint: string) => {
+const fetchCamaraAPI = async (endpoint: string): Promise<IAPIDeputado> => {
     const response = await fetch(`${BASE_URL}${endpoint}`, {
         headers: {
             Accept: "application/json",
         },
     });
-    return await response.json();
+    return await response.json(); // FIXME: Move this to a try/catch block - not here
 };
 
 export default fetchCamaraAPI;

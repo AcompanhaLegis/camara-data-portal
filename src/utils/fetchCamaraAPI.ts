f9713b92f67b322070d6c6becfed40dbbf8b83d4
Kimuraz <j.eduardokimura@icloud.com>
2025-03-21 18:56:43 +0100

    Add error handling on fetchCamaraAPI

--- src/utils/fetchCamaraAPI.ts
+++ src/utils/fetchCamaraAPI.ts
@@ -1,12 +1,36 @@
 import { BASE_URL } from "@/api";
 
+export class CamaraAPIError extends Error {
+    constructor(public response: Response) {
+        super();
+        this.message = this._formatUserError();
+    }
+
+    _formatUserError(): string {
+        switch (this.response.status) {
+        case 404:
+            return "Não encontrado";
+        case 503:
+        case 500:
+            return "Erro interno do servidor. Tente novamente mais tarde.";
+        default:
+            return "Erro desconhecido. Tente novamente mais tarde.";
+        }
+    }
+}
+
 const fetchCamaraAPI = async (endpoint: string) => {
     const response = await fetch(`${BASE_URL}${endpoint}`, {
         headers: {
             Accept: "application/json",
         },
     });
-    return await response.json(); // FIXME: Move this to a try/catch block - not here
+
+    if (!response.ok) {
+        throw new CamaraAPIError(response);
+    }
+
+    return await response.json();
 };
 
 export default fetchCamaraAPI;
